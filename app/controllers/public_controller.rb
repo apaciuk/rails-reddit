c6be950edbdc294dd13c4ce9297c83679244fa61
Xhostcom <xhostcom@gmail.com>
2021-03-14 11:49:03 +0000

    Gitacora saved 2 changed files

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,8 +1,5 @@
 class PublicController < ApplicationController
-      def index
-  
-      @posts = Post.limit(20).sort_by{ |p| p.score }.reverse
-    end
+ 
     def profile
       @profile = Account.find_by_username params[:username]
       @posts = @profile.posts
