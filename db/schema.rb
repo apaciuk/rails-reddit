0edb3bd07f26de60c3f88e749efd678e0a6db138
Xhostcom <xhostcom@gmail.com>
2021-03-14 19:05:23 +0000

    Gitacora saved 1 changed files

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,14 @@ ActiveRecord::Schema.define(version: 2021_03_14_180125) do
   enable_extension "plpgsql"
 
   create_table "communities", force: :cascade do |t|
+    t.bigint "users_id"
     t.string "name"
     t.string "url"
     t.text "rules"
     t.integer "total_members"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_communities_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
