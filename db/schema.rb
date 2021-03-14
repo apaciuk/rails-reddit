1287aeae39222c3d777f7bdb556b56fc66cae30b
Xhostcom <xhostcom@gmail.com>
2021-03-14 18:12:34 +0000

    Update styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_110201) do
+ActiveRecord::Schema.define(version: 2021_03_14_180125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.text "rules"
+    t.integer "total_members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
