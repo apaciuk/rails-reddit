5134ad9bab1db83a56b86f4fe9c3614ac980f7b2
Xhostcom <xhostcom@gmail.com>
2021-03-14 18:59:45 +0000

    Gitacora saved 2 changed files

--- db/migrate/20210314180125_create_communities.rb
+++ db/migrate/20210314180125_create_communities.rb
@@ -1,6 +1,7 @@
 class CreateCommunities < ActiveRecord::Migration[6.1]
   def change
     create_table :communities do |t|
+      t.references :user
       t.string :name
       t.string :url
       t.text :rules
